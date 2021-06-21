3ad12bf318f7d8bfee8a6fd38b1a10f9ad39790c
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2021-06-21 21:29:57 +0100

    Add basic styles and dummy data

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -3,24 +3,70 @@ import styled from "styled-components";
 
 import logoE from "../assets/elmLogoE.png";
 import logoElm from "../assets/elmLogo.png";
+import { AiFillHome } from "react-icons/ai";
 
-const Menu = styled.aside`
+const optionsArray = [
+  { name: "Overview", pages: ["Overview", "Rate of Sale", "Discounts"] },
+  { name: "Amazon", pages: ["Overview", "Promotions"] },
+  { name: "Co-Op", pages: ["Overview", "Promotions"] },
+  { name: "Holland & Barrett", pages: [] },
+  { name: "Morrisons", pages: [] },
+];
+
+const Menu = styled.nav`
   min-height: 100vh;
   height: 100vh;
   min-width: 5rem;
   width: 10%;
   background-color: var(--gray-dark);
+  color: white;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  align-items: center;
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
 `;
 
 const Logo = styled.img`
   margin: 1rem;
-  height: 1.8rem;
+  max-height: 1.8rem;
+  max-width: 1.8rem;
+`;
+
+const SmallOption = styled.li`
+  background-color: var(--gray);
+  height: 1.5rem;
+  width: 1.5rem;
+  margin: 1rem;
+  border-radius: 50%;
+  text-align: center;
+  padding: 0.5rem;
+`;
+
+const HomeOption = styled(AiFillHome)`
+  height: 2rem;
+  width: 2rem;
+  color: var(--gray);
 `;
 
 const Sidebar = () => {
+  const renderOptions = (arrayOfRetailers) => {
+    return arrayOfRetailers.map((el) => {
+      if (el.name.toLowerCase() === "overview") {
+        return <HomeOption />;
+      }
+      return <SmallOption>{el.name.substring(0, 2)}</SmallOption>;
+    });
+  };
+
   return (
     <Menu>
       <Logo src={logoE} />
+      <ul>{renderOptions(optionsArray)}</ul>
     </Menu>
   );
 };
